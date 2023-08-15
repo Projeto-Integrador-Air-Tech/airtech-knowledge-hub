803346640bf8aaeff4681dee3e289cdb35801d36
Vitório Cavalheiro <vitoriocavalheiro03@gmail.com>
2023-08-15 17:38:52 -0300

    feat(doc) dbdocs implementation
    this commit is for the generation of documentation through dbdocs

--- data/airtech.sql
+++ data/airtech.sql
@@ -1,181 +1,169 @@
-CREATE DATABASE airtech;
+CREATE SCHEMA "authorization";
 
+CREATE SCHEMA "management";
 
-SELECT DATABASE airtech;
+CREATE SCHEMA "storage";
 
-CREATE TABLE aircraft_model(
-  id_aircraft_model INT NOT NULL AUTO_INCREMENT,
-  model VARCHAR(50) NOT NULL,
-  manufacturer VARCHAR(50) NOT NULL,
-  manufacturing_year VARCHAR(50) NOT NULL,
-  aircraft_type VARCHAR(50) NOT NULL,
-  passenger_capacity VARCHAR(50) NOT NULL,
-  cargo_capacity VARCHAR(50) NOT NULL,
-  range VARCHAR(50) NOT NULL,
-  maximum_speed VARCHAR(50) NOT NULL,
-  service_ceiling VARCHAR(50) NOT NULL,
-  engine_type VARCHAR(50) NOT NULL,
-  number_of_engines INT NOT NULL,
-  maximum_takeoff_weight VARCHAR(50) NOT NULL,
-  fuel_system VARCHAR(50) NOT NULL,
-  electrical_system VARCHAR(50) NOT NULL,
-  hydraulic_system VARCHAR(50) NOT NULL,
-  landing_gear VARCHAR(50) NOT NULL,
-  avionics_system VARCHAR(50) NOT NULL,
-  certifications TEXT,
-  documentation TEXT,
-  current_market_value DECIMAL(20,2),
-  sales_price_history TEXT,
-  insurance_value DECIMAL(10,2),
-  leasing_history TEXT,
-  image TEXT,
-  PRIMARY KEY (id_aircraft_model)
+CREATE TYPE "status_enum" AS ENUM (
+  'to do',
+  'in progress',
+  'done',
+  'canceled'
 );
 
-CREATE TABLE aircraft (
-    id_aircraft INT NOT NULL AUTO_INCREMENT,
-    model_id INT NOT NULL,
-    registration VARCHAR(10) NOT NULL,
-    serial_number VARCHAR(50) NOT NULL,
-    flight_hours INT NOT NULL,
-    landing_takeoff_cycles INT NOT NULL,
-    routes_flown TEXT,
-    flight_logs TEXT,
-    current_owner VARCHAR(50),
-    owner_history TEXT,
-    notes TEXT,
-    PRIMARY KEY (id_aircraft),
-    FOREIGN KEY (model_id) REFERENCES aircraft_model(id_aircraft_model)
+CREATE TYPE "maintenance_enum" AS ENUM (
+  'corrective maintenance',
+  'preventive maintenance',
+  'predictive maintenance',
+  'emergency maintenance'
 );
 
-CREATE TABLE part_categories (
-    id_part_categories INT NOT NULL AUTO_INCREMENT,
-    category VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    PRIMARY KEY (id_part_categories)
+CREATE TABLE "authorization"."profiles" (
+  "profile_id" SERIAL PRIMARY KEY,
+  "profile_name" VARCHAR(100) NOT NULL
 );
 
-
-CREATE TABLE parts (
-  id_parts INT NOT NULL AUTO_INCREMENT,
-  category_id INT,
-  name VARCHAR(255),
-  part_number VARCHAR(255),
-  serial_number VARCHAR(255),
-  description TEXT,
-  entry_date DATE,
-  price DECIMAL(10, 2),
-  supplier VARCHAR(255),
-  condition VARCHAR(255),
-  quantity INT,
-  PRIMARY KEY (id_parts),
-  FOREIGN KEY (category_id) REFERENCES part_categories(id_part_categories)
+CREATE TABLE "authorization"."permissions" (
+  "permission_id" SERIAL PRIMARY KEY,
+  "permission_name" VARCHAR(100) NOT NULL
 );
 
-
-CREATE TABLE  part_location (
-  id_part_location INT NOT NULL AUTO_INCREMENT,
-  part_id INT,
-  shelf_id INT,
-  corridor VARCHAR(255),
-  height VARCHAR(255),
-  PRIMARY KEY (id_stock_location),
-  FOREIGN KEY (part_id) REFERENCES parts(id_parts)
-); 
-
-CREATE TABLE person (
-    id_person INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    date_of_birth DATE,
-    gender VARCHAR(50),
-    cpf VARCHAR(14),
-    PRIMARY KEY (id_person)
+CREATE TABLE "authorization"."access_profiles" (
+  "access_profiles_id" SERIAL PRIMARY KEY,
+  "profile_id" INT,
+  "permission_id" INT,
+  "access_profiles_name" VARCHAR(100),
+  "access_profiles_description" VARCHAR(150),
+  "access_profile_group_name" VARCHAR(100)
 );
 
-CREATE TABLE position (
-    id_position INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    PRIMARY KEY (id_position)
+CREATE TABLE "authorization"."access_user" (
+  "access_user_id" SERIAL PRIMARY KEY,
+  "user_id" INT,
+  "access_profiles_id" INT,
+  "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE employees (
-id_employee INT NOT NULL AUTO_INCREMENT,
-id_person INT,
-id_job INT,
-employee_email VARCHAR(255) NOT NULL,
-password VARCHAR(255) NOT NULL,
-PRIMARY KEY (id_employee),
-FOREIGN KEY (id_person) REFERENCES person (id_person),
-FOREIGN KEY (id_job) REFERENCES job (id_job)
+CREATE TABLE "authorization"."api_token" (
+  "token" VARCHAR(255),
+  "user_id" INT,
+  "username" VARCHAR(100),
+  "email" VARCHAR(100)
 );
 
-CREATE TABLE external_employees (
-id_external_employee INT NOT NULL AUTO_INCREMENT,
-id_person INT,
-id_job_title INT,
-email VARCHAR(255) NOT NULL,
-password VARCHAR(255) NOT NULL,
-external_company VARCHAR(255) NOT NULL,
-PRIMARY KEY (id_external_employee),
-FOREIGN KEY (id_person) REFERENCES person (id_person),
-FOREIGN KEY (id_job_title) REFERENCES job_title (id_job_title)
+CREATE TABLE "authorization"."users" (
+  "user_id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(100) NOT NULL,
+  "email" VARCHAR(100) NOT NULL,
+  "password_hash" VARCHAR(100) NOT NULL,
+  "full_name" VARCHAR(200),
+  "date_of_birth" DATE,
+  "registration_date" timestamp DEFAULT (CURRENT_TIMESTAMP),
+  "users_status" VARCHAR(20) DEFAULT 'active'
 );
 
-CREATE TABLE accesses (
-    id_accesses INT NOT NULL AUTO_INCREMENT,
-    id_position,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    PRIMARY KEY (id_accesses),
-    FOREIGN KEY (id_position) REFERENCES position (id_position)
+CREATE TABLE "management"."task" (
+  "task_id" SERIAL PRIMARY KEY,
+  "user_id" INT,
+  "aircraft_id" INT,
+  "creation_date" timestamp DEFAULT (CURRENT_TIMESTAMP),
+  "completion_date" DATE,
+  "status" status_enum DEFAULT 'to do'
 );
 
-
-CREATE TABLE tasks (
-id_tasks INT NOT NULL AUTO_INCREMENT,
-id_task_template INT,
-creation_date DATE NOT NULL,
-completion_date DATE,
-status ENUM('to do','open','in progress','delayed','done') NOT NULL,
-responsible_id INT,
-additional_parts INT,
-PRIMARY KEY (id_tasks),
-FOREIGN KEY (responsible_id) REFERENCES employees(id_employees),
-FOREIGN KEY (id_task_template) REFERENCES task_templates(id_task_templates),
-FOREIGN KEY (additional_parts) REFERENCES parts(id_parts)
-);
-CREATE TABLE task_templates(
-  id_task_template INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
-  part_id INT,
-  PRIMARY KEY (id_task_template),
-  FOREIGN KEY (part_id) REFERENCES parts(id_parts)
-);
-
-CREATE TABLE work_order (
-id_work_order INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
-description TEXT,
-start_date DATE NOT NULL,
-end_date DATE,
-status ENUM('to do','open', 'in progress', 'delayed' ,'completed') NOT NULL,
-aircraft_id INT NOT NULL,
-responsible_id INT NOT NULL,
-task_ids VARCHAR(255),
-FOREIGN KEY (aircraft_id) REFERENCES aircraft(id_aircraft),
-FOREIGN KEY (responsible_id) REFERENCES employees(id_employees)
+CREATE TABLE "management"."aircrafts_model" (
+  "aircraft_model_id" SERIAL PRIMARY KEY,
+  "model" VARCHAR(50) NOT NULL,
+  "manufacturer" VARCHAR(50) NOT NULL,
+  "manufacturing_year" VARCHAR(50) NOT NULL,
+  "aircraft_type" VARCHAR(50) NOT NULL,
+  "passenger_capacity" VARCHAR(50) NOT NULL,
+  "cargo_capacity" VARCHAR(50) NOT NULL,
+  "range" VARCHAR(50) NOT NULL,
+  "maximum_speed" VARCHAR(50) NOT NULL,
+  "service_ceiling" VARCHAR(50) NOT NULL,
+  "engine_type" VARCHAR(50) NOT NULL,
+  "number_of_engines" INT NOT NULL,
+  "maximum_takeoff_weight" VARCHAR(50) NOT NULL,
+  "fuel_system" VARCHAR(50) NOT NULL,
+  "electrical_system" VARCHAR(50) NOT NULL,
+  "hydraulic_system" VARCHAR(50) NOT NULL,
+  "landing_gear" VARCHAR(50) NOT NULL,
+  "avionics_system" VARCHAR(50) NOT NULL,
+  "certifications" TEXT,
+  "documentation" TEXT,
+  "leasing_history" TEXT,
+  "image" TEXT
 );
 
-CREATE TABLE token_api (
-    id_funcionarios INT,
-    nome_usuario VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    token_api VARCHAR(255) NOT NULL,
-    descricao VARCHAR(255)
-    FOREIGN KEY (id_funcionarios) REFERENCES funcionarios(id_funcionarios)
+CREATE TABLE "management"."aircrafts" (
+  "aircraft_id" SERIAL PRIMARY KEY,
+  "aircraft_model_id" INT NOT NULL,
+  "registration" VARCHAR(10) NOT NULL,
+  "aircraft_serial_number" VARCHAR(50) NOT NULL,
+  "aircraft_flight_hours" INT NOT NULL,
+  "aircraft_landing_takeoff_cycles" INT NOT NULL,
+  "aircraft_routes_flown" TEXT,
+  "aircraft_flight_logs" TEXT,
+  "aircraft_current_owner" VARCHAR(50),
+  "aircraft_owner_history" TEXT,
+  "aircraft_notes" TEXT
 );
+
+CREATE TABLE "management"."work_order" (
+  "work_order_id" SERIAL PRIMARY KEY,
+  "aircraft_id" INT NOT NULL,
+  "title" VARCHAR(255) NOT NULL,
+  "maintenance" maintenance_enum NOT NULL,
+  "start_date" DATE NOT NULL,
+  "end_date" DATE
+);
+
+CREATE TABLE "storage"."parts" (
+  "parts_id" SERIAL PRIMARY KEY,
+  "parts_name" VARCHAR(100),
+  "parts_description" TEXT,
+  "aircraft_id" INT,
+  "price" "DECIMAL(10, 2)",
+  "supplier" VARCHAR(150),
+  "quantity" INT
+);
+
+CREATE TABLE "storage"."serial_parts" (
+  "serial_parts_id" SERIAL PRIMARY KEY,
+  "parts_id" INT,
+  "serial_number" VARCHAR(100),
+  "parts_weight" "DECIMAL(10, 2)"
+);
+
+CREATE TABLE "storage"."parts_allocation" (
+  "task_id" INT,
+  "serial_parts_id" INT,
+  "quantity" INT
+);
+
+ALTER TABLE "authorization"."access_profiles" ADD FOREIGN KEY ("profile_id") REFERENCES "authorization"."profiles" ("profile_id");
+
+ALTER TABLE "authorization"."access_profiles" ADD FOREIGN KEY ("permission_id") REFERENCES "authorization"."permissions" ("permission_id");
+
+ALTER TABLE "authorization"."access_user" ADD FOREIGN KEY ("access_profiles_id") REFERENCES "authorization"."access_profiles" ("access_profiles_id");
+
+ALTER TABLE "authorization"."access_user" ADD FOREIGN KEY ("user_id") REFERENCES "authorization"."users" ("user_id");
+
+ALTER TABLE "authorization"."api_token" ADD FOREIGN KEY ("user_id") REFERENCES "authorization"."users" ("user_id");
+
+ALTER TABLE "management"."task" ADD FOREIGN KEY ("user_id") REFERENCES "authorization"."users" ("user_id");
+
+ALTER TABLE "storage"."parts" ADD FOREIGN KEY ("aircraft_id") REFERENCES "management"."aircrafts" ("aircraft_id");
+
+ALTER TABLE "management"."task" ADD FOREIGN KEY ("aircraft_id") REFERENCES "management"."aircrafts" ("aircraft_id");
+
+ALTER TABLE "management"."aircrafts" ADD FOREIGN KEY ("aircraft_model_id") REFERENCES "management"."aircrafts_model" ("aircraft_model_id");
+
+ALTER TABLE "management"."work_order" ADD FOREIGN KEY ("aircraft_id") REFERENCES "management"."aircrafts" ("aircraft_id");
+
+ALTER TABLE "storage"."parts_allocation" ADD FOREIGN KEY ("task_id") REFERENCES "management"."task" ("task_id");
+
+ALTER TABLE "storage"."serial_parts" ADD FOREIGN KEY ("parts_id") REFERENCES "storage"."parts" ("parts_id");
+
+ALTER TABLE "storage"."parts_allocation" ADD FOREIGN KEY ("serial_parts_id") REFERENCES "storage"."serial_parts" ("serial_parts_id");
